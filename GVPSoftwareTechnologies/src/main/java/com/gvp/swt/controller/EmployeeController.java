f1035ba7ed9d0e2daf7a059f95e14b9a8c0a11e2
GVP <vndprasadgrandhi@gmail.com>
2020-05-04 00:05:05 +0530

    Added get,update and delete methods

--- GVPSoftwareTechnologies/src/main/java/com/gvp/swt/controller/EmployeeController.java
+++ GVPSoftwareTechnologies/src/main/java/com/gvp/swt/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,19 +22,36 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService empSer;
 
+	//Insert Operation
 	@PostMapping(value = "/insert")//http://localhost:8002/empController/insert
 	public EmployeeDM insert(@RequestBody EmployeeDM empObj) {
 		System.out.println("Inserion values-Emp Controller-insert()");
 		return empSer.insert(empObj);
 	}
 	
+	//Get operation for all records
 	@GetMapping(value = "/getAll")//http://localhost:8002/empController/getAll
 	public Iterable<EmployeeDM> getAll() {
 		return empSer.getAll();
 	}
 	
+	//Get operation for single record
+	@GetMapping(value="/getById/{empCountId}")
+	public EmployeeDM get(@PathVariable("empCountId") int empCount){
+		return empSer.get(empCount);
+	}
+	
+	//Delete operation for single record
 	@DeleteMapping(value = "/delById/{empKey}")//http://localhost:8002/empController/delById/1
 	public void delete(@PathVariable("empKey") int empCount) {
 		 empSer.delete(empCount);;
 	}
+	
+	//Update operation
+	@PutMapping(value="/update/{empCountId}/{empIdStatus}")
+	public EmployeeDM updateTicket(@PathVariable("empCountId") Integer empCount,@PathVariable("empIdStatus") String empIdStatus) {
+		return empSer.update(empCount, empIdStatus);
+	}
+	
+	
 }
--- GVPSoftwareTechnologies/src/main/java/com/gvp/swt/service/EmployeeService.java
+++ GVPSoftwareTechnologies/src/main/java/com/gvp/swt/service/EmployeeService.java
@@ -12,17 +12,32 @@ public class EmployeeService {
 	@Autowired
 	EmployeeDAO empDao;
 
+	//Insertion Operation
 	public EmployeeDM insert(EmployeeDM empObj) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Service");
-		return empDao.save(empObj);//Inserting values
+		return empDao.save(empObj);//Inserting record
 	}
 	
+	//Search operation for all records
 	public Iterable<EmployeeDM> getAll() {
-		return empDao.findAll();
+		return empDao.findAll();//Getting all records
+	}
+	
+	//Search operation for single record
+	public EmployeeDM get(int empCount) {
+		return empDao.findById(empCount).orElse(new EmployeeDM());//Getting single record
 	}
 
+	//Delete operation for single record
 	public void delete(int empCount) {
-		empDao.deleteById(empCount);;
+		empDao.deleteById(empCount);//Deleting record
+	}
+	
+	//Updating operation
+	public EmployeeDM update(int empCount,String empIdStatus) {
+		EmployeeDM empRec = get(empCount);//Getting Record
+		empRec.setEmpIdStatus(empIdStatus);//Setting updated value
+		return empDao.save(empRec);//Saving updated value
 	}
 }
